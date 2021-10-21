5c1ce640be20f9d7c4756aa5da8b440e2ccbaa8f
JJJACY <49667416+JJJACY@users.noreply.github.com>
2021-10-21 16:01:30 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { reactive } from 'vue'
+  const a = reactive({
+    list: [
+      {id:1,name:'劉備'},
+      {id:2,name:'關羽'},
+      {id:3,name:'張飛'}
+    ]
+  })
+  const handleClickItem = (item) =>{
+   console.log(item,11)
+  }
+  defineExpose({
+    a,
+    handleClickItem
+  })
+</script>
+
+<template>
+  <div class="page-home">
+    首页
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
